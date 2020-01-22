72ba8ea38f13646e0c7d01ae24a0e72ef41ab007
Joaquin Bono <joaquinbono.18@campuscamara.es>
2020-01-22 18:24:08 +0100

    [adds cors to server]

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const { mongoose } = require('./database');
+const cors = require('cors');
 
 //Settings
 app.set('port', process.env.PORT || 3000);
@@ -9,6 +10,7 @@ app.set('port', process.env.PORT || 3000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({origin:'http://localhost:4200'}));
 
 //Routes
 app.use('/api/teachers', require('./routers/teachers.routes'));
